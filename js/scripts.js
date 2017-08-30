c84040777c258155c435cda646cb13a3d9a8f65b
mib1092 <mazurenko.ihor@gmail.com>
2017-08-30 18:50:57 +0300

    FAQ

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,41 @@ jQuery(document).ready(function($) {
 
     // the blocking of links default behavior
     function prevent(){
-        $('.prevent, .mobile-menu .expanded, a[href="#"]').on('click', function(event){
+        $('.prevent, .mobile-menu .expanded, a[href="/broken"]').on('click', function(event){
             event.preventDefault();
         });
     }
     prevent();
+
+    // for FAQ list
+    $('.faq-sub-list > li').each(function() {
+        var $this = $(this),
+            h2    = $this.children('h2');
+
+        // create unique id for a11y relationship
+        var id = 'collapsible-' + $this.index();
+
+        // wrap the content and make it focusable
+        $this.find('.accordion-box-content').wrapAll('<div id="'+ id +'" aria-hidden="true">');
+        var panel = h2.next();
+
+        // Add the button inside the <h2> so both the heading and button semanics are read
+        h2.wrapInner('<a href="/broken" class="prevent" aria-expanded="false" aria-controls="'+ id +'">');
+
+        // Toggle the state properties
+        h2.on('click', function(e) {
+            e.preventDefault();
+            var a     = $(this).find('a'),
+                state = ( a.attr('aria-expanded') === 'false' ) ? true : false;
+            a.attr('aria-expanded', state);
+            panel.attr('aria-hidden', !state);
+            if (state) {
+                $(this).addClass('open');
+            } else {
+                $(this).removeClass('open');
+            }
+        });
+    });
 
     // for header search toggle
     $(".search-btn, .close-search").on('click', function(){
