1a7730160825f02f150205867d9a3db0d908e14a
mib1092 <mazurenko.ihor@gmail.com>
2017-09-01 20:10:17 +0300

    mib

--- js/scripts.js
+++ js/scripts.js
@@ -55,12 +55,4 @@ jQuery(document).ready(function($) {
         $(document.body).toggleClass('overflow');
     });
 
-    // for smooth scroll
-    smoothScroll.init({
-        selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
-        speed: 500, // Integer. How fast to complete the scroll in milliseconds
-        easing: 'easeInQuad', // Easing pattern to use
-        offset: 50 // Integer. How far to offset the scrolling anchor location in pixels
-    });
-
 });
